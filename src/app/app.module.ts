6ca0f065890ddeb33f0cc27e5affc6524f6c5d9b
Domenico Valentino <domenico2727@gmail.com>
2023-01-16 21:28:20 -0500

    work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './components/details/details.component';
+import { DividerComponent } from './components/divider/divider.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    DividerComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/details/details.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit {
+  title = "{ }";
+  title1 = "< />";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
